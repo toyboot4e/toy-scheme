3a7db7a85ef64b8a30d81344c976846104550f46
toyboot4e <toyboot4e@gmail.com>
2021-06-03 18:35:38 +0900

    add boilerplate

---
+++ toy-scheme.el
@@ -0,0 +1,19 @@
+;; Author: toyboot4e <toyboot4e@gmail.com>
+;; Version: 0.0.0
+;; Homepage: https://github.com/toyboot4e/toy-scheme
+;; Keywords: 
+;; Package-Requires: ((emacs "26.1"))
+;; License: MIT
+
+;;; Commentary:
+
+;; `toy-scheme.el' is a package for handling custom path schemes in `schemes.txt'.
+
+;;; Code:
+
+;; Local variables:
+;; lisp-body-indent: 4
+;; indent-tabs-mode: nil
+;; END
+
+;;; toy-scheme.el ends here
